8d33432529af0835b1a8a34594f6baca24ff4c40
Víctor Quirino <victorsouzaq@gmail.com>
2024-03-14 17:13:25 -0300

    Exercicio 2 da prática 2

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,11 @@
+﻿object o = null;
+
+try
+{
+    o.ToString();
+
+}
+catch (NullReferenceException e)
+{
+    Console.WriteLine("Seu objeto tem um referencia nula. Instancie seu objeto primeiro.");
+}
