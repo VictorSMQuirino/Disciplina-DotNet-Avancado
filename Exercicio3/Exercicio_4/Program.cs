885e3e0bb5f19c1fbadef88725c17c73f0156f6e
Víctor Quirino <victorsouzaq@gmail.com>
2024-03-20 20:23:55 -0300

    Exercício 4 da prática 3

---
+++ Exercicio3/Exercicio_4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var task1 = DoWorkAsync("Task 1", 10, 1000);
+var task2 = DoWorkAsync("Task 2", 5, 2500);
+
+await Task.WhenAll(task1, task2);
+
+Console.WriteLine("Todas as taks foram concluídas.");
+
+static async Task DoWorkAsync(string name, int iterations, int delay)
+{
+    for (var i = 1; i <= iterations; i++)
+    {
+        Console.WriteLine($"{name} - Iteração: {i}");
+        await Task.Delay(delay);
+    }
+    Console.WriteLine($"Task {name} concluída.");
+}
